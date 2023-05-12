879d6c4ed9c3272a1a4236df82c3da7d0c752d31
Carlos <carlosng1993@gmail.com>
2023-05-12 11:08:12 +0200

    Feat: Server changed with postgresql

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,34 @@
-﻿require('rootpath')();
-const express = require('express');
+﻿require("rootpath")();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-const errorHandler = require('_middleware/error-handler');
+const errorHandler = require("_middleware/error-handler");
+const sequelize = require("sequelize");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // api routes
-app.use('/users', require('./users/users.controller'));
+app.use("/users", require("./users/users.controller"));
 
 // global error handler
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
-app.listen(port, () => console.log('Server listening on port ' + port));
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 3000;
+app.listen(port, () => console.log("Server listening on port " + port));
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log(`Database & tables created!`);
+    app.listen(3000, () =>
+      console.log(`Server running at http://localhost:3000`)
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- Backend/users/user.model.js
+++ Backend/users/user.model.js
@@ -1,25 +1,54 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = model;
 
 function model(sequelize) {
-    const attributes = {
-        firstName: { type: DataTypes.STRING, allowNull: false },
-        lastName: { type: DataTypes.STRING, allowNull: false },
-        username: { type: DataTypes.STRING, allowNull: false },
-        hash: { type: DataTypes.STRING, allowNull: false }
-    };
+  const users = {
+    Id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: { type: DataTypes.STRING, allowNull: false },
+    lastName: { type: DataTypes.STRING, allowNull: false },
+    username: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    hash: { type: DataTypes.STRING, allowNull: false },
+    number_absence: { type: DataTypes.INTEGER, allowNull: false },
+  };
 
-    const options = {
-        defaultScope: {
-            // exclude hash by default
-            attributes: { exclude: ['hash'] }
-        },
-        scopes: {
-            // include hash with this scope
-            withHash: { attributes: {}, }
-        }
-    };
+  const vacations = {
+    Id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    start_date: { type: DataTypes.DATE, allowNull: false },
+    end_date: { type: DataTypes.DATE, allowNull: false },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "Id",
+      },
+      state: { type: DataTypes.STRING, allowNull: false },
+    },
+  };
 
-    return sequelize.define('User', attributes, options);
+  const options = {
+    defaultScope: {
+      // exclude hash by default
+      users: { exclude: ["hash"] },
+      vacations: { exclude: ["hash"] },
+    },
+    scopes: {
+      // include hash with this scope
+      withHash: { users: {}, vacations: {} },
+    },
+  };
+
+  return sequelize.define("User", attributes, options);
 }
